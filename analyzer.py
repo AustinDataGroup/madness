4abba2706720c6dc545499a1c279b124fde7c5a8
David Brear <davidbrear04@gmail.com>
2014-02-16 12:47:02 -0600

    added some output lines to show where we are in the process

---
+++ analyzer.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import numpy as np
+import csv
+from sklearn.ensemble import RandomForestClassifier
+
+csv_reader = csv.reader(open('data/regular_season_labeled_data.csv', 'rb'))
+test_data_csv = csv.reader(open('data/testing_data.csv'))
+
+test_data_csv.next()
+print csv_reader.next()
+
+print 'Loading the training data...',
+data = []
+for row in csv_reader:
+    data.append(row)
+print 'done!'
+
+print 'Loading the testing data...',
+test_data = []
+for row in test_data_csv:
+    test_data.append(row)
+print 'done!'
+
+data = np.array(data)
+test_data = np.array(test_data)
+
+Forest = RandomForestClassifier(n_estimators = 100)
+
+print 'Fitting trees with the training data...',
+Forest = Forest.fit(data[0:, 1:], data[0:, 0])
+print 'done!'
+
+print 'Predicting test data...',
+Output = Forest.predict(test_data)
+print 'done!'
+
+print 'Writing predictions...',
+outfile = csv.writer(open('predictions.csv', 'wb'))
+outfile.writerow(['ndx', 'team1_won'])
+for ndx, row in enumerate(Output):
+    outfile.writerow([ndx+1, row])
+print 'done!'
